3d64a8df205a0738cac8042eeeb19598d19ff628
StudioEtrange <nomorgan@gmail.com>
2015-02-08 00:35:42 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# goconfig-cli
+
+command line tool reading ini file
+
+based on goconfig https://github.com/Unknwon/goconfig
+
+# Install
+
+	go get github.com/StudioEtrange/goconfig-cli
+
+# Usage
+
+	$GOPATH/bin/goconfig-cli <ini-file> getkey <section> <key>
+	$GOPATH/bin/goconfig-cli <ini-file> setkey <section> <key> <value>
+	$GOPATH/bin/goconfig-cli <ini-file> delkey <section> <key>
---
+++ main.go
@@ -0,0 +1,104 @@
+package main
+
+
+
+import (
+	"fmt"
+	"github.com/Unknwon/goconfig"
+	"os"
+	//TODO test : "gopkg.in/ini.v1"
+	"gopkg.in/codegangsta/cli.v1"
+)
+
+var fileName string
+var configFile *goconfig.ConfigFile
+var modified bool = false
+
+func main() {
+
+	prettyFlag := cli.BoolFlag{
+		Name:  "pretty, p",
+		Usage: "active space around = character",
+	}
+
+	// main controller
+	app := cli.NewApp()
+	app.Name = "goconfig-cli"
+	app.Usage = "simple cli to manipulate ini file"
+	app.Before = func(c *cli.Context) error {
+		fileName = c.Args().Get(1)
+		return Init(fileName)
+	}
+	app.Commands = []cli.Command{
+		{
+			Name:      "getkey",
+			ShortName: "g",
+			Usage:     "get key value of a section",
+			Action: func(c *cli.Context) {
+				val := GetKey(c.Args().Get(1), c.Args().Get(2))
+				fmt.Print(val)
+			},
+		},
+
+		{
+			Name:      "delkey",
+			ShortName: "d",
+			Usage:     "delete a key from a section",
+			Flags: []cli.Flag{
+				prettyFlag,
+			},
+			Action: func(c *cli.Context) {
+				goconfig.PrettyFormat = c.Bool("pretty")
+				DelKey(c.Args().Get(1), c.Args().Get(2))
+			},
+		},
+
+		{
+			Name:      "setkey",
+			ShortName: "s",
+			Usage:     "add/replace a key with a value into a section",
+			Flags: []cli.Flag{
+				prettyFlag,
+			},
+			Action: func(c *cli.Context) {
+				goconfig.PrettyFormat = c.Bool("pretty")
+				SetKey(c.Args().Get(1), c.Args().Get(2), c.Args().Get(3))
+			},
+		},
+	}
+
+	// launch app
+	app.RunAndExitOnError()
+
+	// close app
+	if modified {
+		if err := goconfig.SaveConfigFile(configFile, fileName); err != nil {
+			os.Exit(1)
+		}
+	}
+	os.Exit(0)
+}
+
+func Init(filename string) error {
+	var err error
+	configFile, err = goconfig.LoadConfigFile(filename)
+	return err
+}
+
+func GetKey(section, key string) string {
+	val, err := configFile.GetValue(section, key)
+	if err != nil {
+		return ""
+	}
+	return val
+}
+
+func DelKey(section, key string) {
+	configFile.DeleteKey(section, key)
+	modified = true
+}
+
+func SetKey(section, key, val string) {
+	configFile.SetValue(section, key, val)
+	modified = true
+}
